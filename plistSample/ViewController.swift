fe0228b81e3dac63744124a798a801a01c6f398c
hayashidesu <katsu-h@w4.dion.ne.jp>
2015-06-12 13:42:13 +0900

    Initial Commit

---
+++ plistSample/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  plistSample
+//
+//  Created by 林克彦 on 2015/06/12.
+//  Copyright (c) 2015年 Hayashidesu. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
